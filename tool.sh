ad8bffdb88033c8e50de0d10cde8a7ea93e85916
xyzwj <xyz-wj@163.com>
2019-09-27 00:10:27 +0800

    mod copy.sh

--- tool.sh
+++ tool.sh
@@ -2,17 +2,17 @@ mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo  http://mirrors.aliyun.com/repo/Centos-7.repo
 yum make cache
 
-wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.zip
-unzip boost_1_66_0.zip
+#wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.zip
+#unzip boost_1_66_0.zip
 
-yum intall gcc gcc-c++ python-devel cmake -y
+#yum intall gcc gcc-c++ python-devel cmake -y
 
-cd boost_1_66_0
+#cd boost_1_66_0
 
-./bootstrap.sh –with-libraries=all –with-toolset=gcc -with-python=/usr/bin/python2.7
+#./bootstrap.sh –with-libraries=all –with-toolset=gcc -with-python=/usr/bin/python2.7
 
-./b2 toolset=gcc
-./b2 install --prefix=/usr
+#./b2 toolset=gcc
+#./b2 install --prefix=/usr
 #ldconfig -v
 
 #git clone https://github.com/xyzwj/xtp_api_python.git
