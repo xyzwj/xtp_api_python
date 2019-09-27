e62bc8ee3c928755d21b342efa3b29a9659d3522
xyzwj <xyz-wj@163.com>
2019-09-27 20:39:05 +0800

    mod copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
-#cp bin/Linux/Linux+python2.7/*.so /usr/lib64/python2.7/site-packages/
+cp bin/Linux/Linux+python2.7/*.so /usr/lib64/python2.7/site-packages/
 #cp /root/xtp_api_python/boost_1_66_0/stage/lib/* /usr/lib64/python2.7/site-packages/
 cp ./source/Linux/xtp_python2_18.19/xtpapi/*.so /usr/lib/
 cp ./boost_1_66_0/stage/lib/* /usr/lib/
